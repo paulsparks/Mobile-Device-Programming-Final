6aea9fb41648dff846f6842e7c72da75fedc8afa
paulsparks <psparks1225@gmail.com>
2024-04-17 17:55:20 -0400

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BearcatWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BearcatWeather
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
