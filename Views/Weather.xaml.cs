d0936f8b9c54af1deed7afac85d64ad0d35e909c
sasidhar chilaka <97551417+chilaksr098@users.noreply.github.com>
2024-04-21 18:39:20 -0400

    Update buttons Weather.xaml.cs

--- Views/Weather.xaml.cs
+++ Views/Weather.xaml.cs
@@ -13,5 +13,17 @@ namespace BearcatWeather.Views
             ViewModel.InitializeAsync();
             BindingContext = ViewModel;
         }
+        
+        private async void ShareButton_Clicked(object sender, EventArgs e)
+        {
+            // Implement share functionality here
+            await DisplayAlert("Share", "Weather information shared!", "OK");
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            // Implement save functionality here
+            await DisplayAlert("Save", "Weather information saved!", "OK");
+        }
     }
 }
